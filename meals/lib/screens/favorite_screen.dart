0523ff3033e64e19dcdda7d19d392668cf60aba7
Bruno Pivoto <brunorangel@gec.inatel.br>
2023-05-18 15:58:31 -0300

    add filter, settings

--- meals/lib/screens/favorite_screen.dart
+++ meals/lib/screens/favorite_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
 
 class FavoriteScreen extends StatelessWidget {
-  const FavoriteScreen({super.key});
-
+  const FavoriteScreen({super.key, required this.favoriteMeals});
+  final List<Meal> favoriteMeals;
+  
   @override
   Widget build(BuildContext context) {
     return const Center(
--- meals/lib/screens/tabs_screen.dart
+++ meals/lib/screens/tabs_screen.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:meals/components/main_drawer.dart';
-import 'package:meals/screens/categories__screen.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/favorite_screen.dart';
 
 class TabsScreen extends StatefulWidget {
-  const TabsScreen({super.key});
+  const TabsScreen({required this.favoriteMeals, super.key});
 
+  final List<Meal> favoriteMeals;
   @override
   State<TabsScreen> createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedScreenIndex = 0;
-  final List<Map<String, Object>> _screens = [
-    {
-      'title': 'Lista de Categorias',
-      'screen': const CategoriesScreen(),
-    },
-    {
-      'title': 'Lista de Favoritos',
-      'screen': const FavoriteScreen(),
-    },
-  ];
+  late List<Map<String, Object>> _screens;
+
+  @override
+  void initState() {
+    super.initState();
+    _screens = [
+      {
+        'title': 'Lista de Categorias',
+        'screen': const CategoriesScreen(),
+      },
+      {
+        'title': 'Lista de Favoritos',
+        'screen': FavoriteScreen(favoriteMeals: widget.favoriteMeals),
+      },
+    ];
+  }
 
   _selectScreen(int index) {
     setState(() {
